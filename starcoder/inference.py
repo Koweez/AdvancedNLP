6b453dcb2cb1dbfc6c88f6f6d59135b6602ee9ee
elias.tebbani <elias.tebbani@epita.fr>
2024-12-05 19:00:06 +0100

    tinyfix: removed useless variables and prints

--- starcoder/inference.py
+++ starcoder/inference.py
@@ -1,12 +1,9 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from typing import Optional
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 ACCESS_TOKEN = "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"
 checkpoint = "bigcode/starcoderbase-1b"
-device = "cuda" # for GPU usage or "cpu" for CPU usage
 
 tokenizer = AutoTokenizer.from_pretrained(checkpoint, token=ACCESS_TOKEN, cache_dir="/tmp/starcoder")
 tokenizer.pad_token = tokenizer.eos_token
@@ -15,13 +12,11 @@ model.generation_config.pad_token_id = tokenizer.eos_token_id
 
 def postprocess_output(prediction):
     txt = prediction.split('<fim_middle>')[1].split('<fim_suffix>')[0]
-    # removing trailing <|endoftext|>
-    txt = txt[:-13]
+    txt = txt[:-13] # removing trailing <|endoftext|>
     return txt
 
 def fill_in(before_cursor: str, after_cursor: str, device: Optional[str | torch.device] = "cpu") -> str:
     input_text = f"<fim_prefix>{before_cursor}<fim_suffix>{after_cursor}<fim_middle>"
-    print(input_text)
     inputs = tokenizer(input_text, return_tensors="pt", padding=True, return_attention_mask=True)
     
     input_ids = inputs['input_ids'].to(device)
